4f6550bffddd18290d00dccfce8d93e88a0f198d
Peter Hagenson <p.hagenson@gmail.com>
2022-07-05 16:34:09 -0500

    added addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,19 @@
-console.log('***** Music Collection *****')
+console.log("***** Music Collection *****");
+
+//Add a function named `addToCollection."
+let collection = [];
+
+addToCollection = (title, artist, yearPublished) => {
+  let album = {
+    title: title,
+    artist: artist,
+    published: yearPublished,
+  };
+  collection.push(album);
+  return collection[collection.length - 1];
+};
+
+console.log(addToCollection("Layla", "Eric Clapton", 1971));
+console.log(collection);
+
+//
