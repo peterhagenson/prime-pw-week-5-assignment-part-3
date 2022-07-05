2a27fa40640e6cd521cd6f2445068d5125e1cb7c
Peter Hagenson <p.hagenson@gmail.com>
2022-07-05 17:16:33 -0500

    tested addToCollection function and created showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -13,7 +13,37 @@ addToCollection = (title, artist, yearPublished) => {
   return collection[collection.length - 1];
 };
 
-console.log(addToCollection("Layla", "Eric Clapton", 1971));
+console.log(
+  addToCollection(
+    "Layla and Other Assorted Love Songs",
+    "Derek and the Dominos",
+    1971
+  )
+);
+console.log(collection);
+
+//Test the `addToCollection` function:
+console.log(addToCollection("The White Album", "The Beatles", 1968));
+console.log(addToCollection("Harvest", "Neil Young", 1972));
+console.log(addToCollection("Jason Isbell", "Southeastern", 2013));
+console.log(
+  addToCollection("Car Wheels on a Gravel Road", "Lucinda Williams", 1998)
+);
+console.log(addToCollection("Oh Mercy", "Bob Dylan", 1989));
+console.log(
+  addToCollection("Full Moon Fever", "Tom Petty and the Heartbreakers", 1989)
+);
+
 console.log(collection);
 
 //
+showCollection = (array) => {
+  console.log(array.length);
+  for (let item of array) {
+    console.log(
+      `${item.title} by ${item.artist} published in ${item.published}`
+    );
+  }
+};
+
+showCollection(collection);
