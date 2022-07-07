a5ecfe375688a420f7d84ceec0babc48b6f8f7cb
Peter Hagenson <p.hagenson@gmail.com>
2022-07-07 08:03:28 -0500

    added track numbers to output in showCollection function; added tracks array to Sgt. Pepper album

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -36,7 +36,31 @@ console.log(
 console.log(collection);
 
 //Test the `addToCollection` function:
-console.log(addToCollection("The White Album", "The Beatles", 1968));
+console.log(
+  addToCollection(
+    "Sgt. Pepper's Lonley Hearts Club Band",
+    "The Beatles",
+    1967,
+    [
+      { name: "Sgt. Pepper's Lonley Hearts Club Band", duration: "2:00" },
+      { name: "With a Little Help from My Friends", duration: "2:42" },
+      { name: "Lucy in the Sky with Diamonds", duration: "3:28" },
+      { name: "Getting Better", duration: "2:48" },
+      { name: "Fixing a Hole", duration: "2:36" },
+      { name: "She's Leaving Home", duration: "3:25" },
+      { name: "Being for the Benefit of Mr. Kite!", duration: "2:37" },
+      { name: "Within You Without You", duration: "5:05" },
+      { name: "When I'm Sixty-Four", duration: "2:37" },
+      { name: "Lovely Rita", duration: "2:42" },
+      { name: "Good Morning Good Morning", duration: "2:42" },
+      {
+        name: "Sgt. Pepper's Lonely Hearts Club Band (Reprise)",
+        duration: "1:18",
+      },
+      { name: "A Day in the Life", duration: "5:38" },
+    ]
+  )
+);
 console.log(addToCollection("Harvest", "Neil Young", 1972));
 console.log(addToCollection("Let It Be", "The Beatles", 1970));
 console.log(
@@ -52,15 +76,19 @@ console.log(collection);
 // Write showCollection function
 
 //still need to add number before track name
+
 showCollection = (collection) => {
   console.log(collection.length);
   for (let record of collection) {
+    //for (i = 0; i < collection.length; i++) {
     console.log(
       `${record.title} by ${record.artist} published in ${record.year}`
     );
     if (record.tracks) {
+      i = 0;
       for (let track of record.tracks) {
-        console.log(track.name + ": " + track.duration);
+        i++;
+        console.log(i + ". " + track.name + ": " + track.duration);
       }
     }
   }
